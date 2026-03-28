8b7693b24ef08901b6af9861fd7e36eeb1712499
Inzamamkhan786 <151707845+Inzamamkhan786@users.noreply.github.com>
2026-03-28 20:10:54 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,15 +7,22 @@ const authRoutes = require("./Routes/authRoutes");
 const objectRoutes = require("./Routes/objectRoutes");
 const fileRoutes = require("./Routes/fileRoutes");
 const billingRoutes = require("./Routes/billingRoutes");
+const user_detail = require("./Routes/user");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.use((req,res,next)=>{
+  console.log(req.method, req.url);
+  next();
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/files", objectRoutes);   // list files
 app.use("/api/files", fileRoutes);     // upload/download/delete
 app.use("/api/billing", billingRoutes);
+app.use("/api/user/",user_detail);
 
 app.get("/", async (req, res) => {
     const result = await pool.query("SELECT NOW()");
