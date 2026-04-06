c06f2ec4cf02a4a0a7ead0b78fb1bd83b2882eeb
Inzamamkhan786 <151707845+Inzamamkhan786@users.noreply.github.com>
2026-04-07 00:04:01 +0530

    Update fileRoutes.js

--- Backend/Routes/fileRoutes.js
+++ Backend/Routes/fileRoutes.js
@@ -1,41 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const path = require("path");
-const fs = require("fs");
+
 const rateLimiter = require("../Middleware/rateLimiter");
 const authMiddleware = require("../Middleware/authMiddleware");
 const fileController = require("../Controllers/fileController");
 
-const storage = multer.memoryStorage({
-
-    destination: function (req, file, cb) {
-
-        const userId = req.user.userId;
-
-        const baseFolder = path.join(__dirname, "..", "Uploads", userId.toString());
-
-        const uploadFolder = path.join(baseFolder, "uploads");
-        const recycleFolder = path.join(baseFolder, "recycleBin");
-
-        [uploadFolder, recycleFolder].forEach(folder => {
-            if (!fs.existsSync(folder)) {
-                fs.mkdirSync(folder, { recursive: true });
-            }
-        });
-
-        cb(null, uploadFolder);
-    },
-
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
-    }
-
+// memory storage for supabase upload
+const upload = multer({
+    storage: multer.memoryStorage()
 });
 
-const upload = multer({ storage });
-
-router.post("/upload", authMiddleware,rateLimiter, upload.single("file"), fileController.uploadFile);
+router.post(
+  "/upload",
+  authMiddleware,
+  rateLimiter,
+  upload.single("file"),
+  fileController.uploadFile
+);
 
 router.get("/download/:id", authMiddleware, fileController.downloadFile);
 
@@ -45,11 +27,10 @@ router.delete("/:id", authMiddleware, fileController.deleteFile);
 
 router.get("/duplicates", authMiddleware, fileController.getDuplicates);
 
-router.get("/recycle-bin",authMiddleware,fileController.getRecycleBin);
+router.get("/recycle-bin", authMiddleware, fileController.getRecycleBin);
 
-router.post("/restore/:id",authMiddleware,fileController.restoreFile);
+router.post("/restore/:id", authMiddleware, fileController.restoreFile);
 
-router.delete("/permanent-delete/:id",authMiddleware,fileController.permanentDelete);
-
+router.delete("/permanent-delete/:id", authMiddleware, fileController.permanentDelete);
 
 module.exports = router;
