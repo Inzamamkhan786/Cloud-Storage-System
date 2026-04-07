e557b1633779547aa7f4c07a6ac59d2d99b0cd41
Inzamamkhan786 <151707845+Inzamamkhan786@users.noreply.github.com>
2026-04-07 08:48:52 +0530

    Update db.js

--- Backend/Models/db.js
+++ Backend/Models/db.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
 require("dotenv").config();
+
 console.log("DATABASE_URL:", process.env.DATABASE_URL ? "Loaded" : "Missing");
 
 const pool = new Pool({
@@ -8,5 +9,13 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
+
+pool.connect()
+  .then(() => {
+    console.log("✅ PostgreSQL Connected Successfully");
+  })
+  .catch((err) => {
+    console.error("❌ Database Connection Error:", err);
+  });
 
 module.exports = pool;
